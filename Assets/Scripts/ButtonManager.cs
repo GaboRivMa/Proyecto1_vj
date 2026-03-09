9de9e36ba083628138ac0c3ecb4e3a444114e0b4
GaboRivMa <gabrielrivema@gmail.com>
2026-03-08 20:31:20 -0600

    Final

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -26,4 +26,13 @@ public class ButtonManager : MonoBehaviour
     public void GoToComoCredito(){
         SceneManager.LoadScene("Creditos");
     }
+    
+    public void Quit()
+    {
+        // Esto cierra el juego cuando ya está instalado (Build)
+        Application.Quit();
+
+        // Esto es opcional, solo para ver en la consola de Unity que funciona
+        Debug.Log("El juego se está cerrando...");
+    }
 }
